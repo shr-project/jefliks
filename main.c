a154045420ee1f3146e3e01a59e05339efe22ee8
PhoeniX11 Kayo <phoenix11@users.sf.net>
2010-02-04 23:41:46 +0500

    UI Main added..

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include<Elementary.h>
 
-#include"ui_config.h"
-
+#include"ui_main.h"
 
 
 /* this is your elementary main function - it MUSt be called IMMEDIATELY
@@ -9,9 +8,10 @@
  * elm_main and not be static - must be a visible symbol with EAPI infront */
 
 static void
-main_win_del(void *data, Evas_Object *obj, void *event_info){
-   /* called when my_win_main is requested to be deleted */
-   elm_exit(); /* exit the program's main loop that runs in elm_run() */
+main_win_del(void *data, Evas_Object *obj, void *event_info)
+{
+  /* called when my_win_main is requested to be deleted */
+  elm_exit(); /* exit the program's main loop that runs in elm_run() */
 }
 
 /*
@@ -37,7 +37,7 @@ main_win_theme(){
 
 #ifndef TEST_WIDGET_MODE
 int main(int argc, char **argv){
-  Evas_Object *wn, *bg, *rl, *cf, *pc, *bx, *bs;
+  Evas_Object *wn, *bg, *fm;
   //const char *tf;
   
   eet_init();
@@ -57,64 +57,11 @@ int main(int argc, char **argv){
   evas_object_size_hint_weight_set(bg, 1.0, 1.0);
   evas_object_show(bg);
   
-  /* Main box */
-  bx = elm_box_add(wn);
-  evas_object_size_hint_weight_set(bx, 1.0, 1.0);
-  evas_object_size_hint_align_set(rl, -1.0, -1.0);
-  elm_win_resize_object_add(wn, bx);
-  evas_object_show(bx);
-  
-  /* Roster */
-  rl = elm_anchorview_add(wn);
-  evas_object_size_hint_weight_set(rl, 1.0, 1.0);
-  evas_object_size_hint_align_set(rl, -1.0, -1.0);
-  elm_anchorview_text_set(rl, "It's startup <a href=1>kayo@neko.im</a>");
-  //elm_win_resize_object_add(wn, rl);
-  elm_box_pack_end(bx, rl);
-  evas_object_show(rl);
-  
-  /* Buttons */
-  bs = elm_box_add(wn);
-  elm_box_horizontal_set(bs, 1);
-  elm_box_homogenous_set(bs, 1);
-  evas_object_size_hint_weight_set(bs, 1.0, 0.0);
-  evas_object_size_hint_align_set(bs, -1.0, 0.0);
-  elm_box_pack_end(bx, bs);
-  evas_object_show(bs);
-  
-  /* Status */
-  {
-    Evas_Object *sb = elm_hoversel_add(wn);
-    elm_hoversel_label_set(sb, _("Status"));
-    elm_hoversel_hover_parent_set(sb, bx);
-    evas_object_size_hint_weight_set(bs, 1.0, 1.0);
-    evas_object_size_hint_align_set(bs, -1.0, 0.0);
-    
-    elm_hoversel_item_add(sb, _("Offline"), NULL, 0, NULL, NULL);
-    elm_hoversel_item_add(sb, _("Online"), NULL, 0, NULL, NULL);
-    elm_hoversel_item_add(sb, _("Away"), NULL, 0, NULL, NULL);
-    elm_hoversel_item_add(sb, _("Extended Away"), NULL, 0, NULL, NULL);
-    elm_hoversel_item_add(sb, _("Do not disturb"), NULL, 0, NULL, NULL);
-    
-    elm_box_pack_end(bs, sb);
-    evas_object_show(sb);
-  }
-
-  /* Actions */
-  {
-    Evas_Object *ab = elm_hoversel_add(wn);
-    elm_hoversel_label_set(ab, _("Actions"));
-    elm_hoversel_hover_parent_set(ab, bx);
-    evas_object_size_hint_weight_set(bs, 1.0, 1.0);
-    evas_object_size_hint_align_set(bs, -1.0, 0.0);
-    
-    elm_hoversel_item_add(ab, _("Settings"), NULL, 0, NULL, NULL);
-    elm_hoversel_item_add(ab, _("About"), NULL, 0, NULL, NULL);
-    elm_hoversel_item_add(ab, _("Exit"), NULL, 0, NULL, NULL);
-    
-    elm_box_pack_end(bs, ab);
-    evas_object_show(ab);
-  }
+  /* Main */
+  fm = elm_jabber_main(wn);
+  elm_win_resize_object_add(wn, fm);
+  evas_object_size_hint_weight_set(wn, 1.0, 1.0);
+  evas_object_show(fm);
   
   /*
   pc = elm_pager_add(wn);
@@ -122,13 +69,6 @@ int main(int argc, char **argv){
   evas_object_show(pc);
   */
   
-  /*
-  cf = elm_jabber_config_add(wn);
-  evas_object_size_hint_weight_set(cf, 1.0, 1.0);
-  elm_pager_content_push(pc, cf);
-  elm_win_resize_object_add(wn, cf);
-  evas_object_show(cf);
-  */
   //evas_object_hide(cf);
   
   //elm_pager_content_promote(pc, rl);
---
+++ ui_main.c
@@ -0,0 +1,133 @@
+/** ui_main.c --- 
+ *
+ * Copyright (C) 2010 PhoeniX11 Kayo
+ *
+ * Author: PhoeniX11 Kayo <phoenix11@users.sf.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include<Elementary.h>
+
+#include"ui_common.h"
+#include"ui_main.h"
+#include"ui_config.h"
+
+typedef struct _Widget_Data Widget_Data;
+struct _Widget_Data{
+  Evas_Object *parent, *box, *status;
+};
+
+static void
+_del_hook(void *data, Evas *e, Evas_Object *obj, void *event_info)
+{
+  Widget_Data *wd=data;
+  free(wd);
+}
+
+static void
+_exit_hook(void *data, Evas_Object *obj, void *event_info)
+{
+  elm_exit();
+}
+
+static void
+_config_del(void *data, Evas *e, Evas_Object *obj, void *event_info)
+{
+  Widget_Data *wd=data;
+  evas_object_show(wd->box);
+}
+
+static void
+_config_hook(void *data, Evas_Object *obj, void *event_info)
+{
+  Widget_Data *wd=data;  
+  Evas_Object *settings = elm_jabber_config_add(wd->parent);
+  evas_object_size_hint_weight_set(settings, 1.0, 1.0);
+  elm_win_resize_object_add(wd->parent, settings);
+  evas_object_hide(wd->box);
+  evas_object_show(settings);
+  evas_object_event_callback_add(settings, EVAS_CALLBACK_FREE, _config_del, wd);
+}
+
+Evas_Object *elm_jabber_main(Evas_Object *parent){
+  Widget_Data *wd;
+  Evas_Object *box, *buttons, *status, *actions, *rl;
+  
+  wd = malloc(sizeof(Widget_Data));
+  wd->parent=parent;
+  
+  /* Main box */
+  box = elm_box_add(parent);
+  wd->box=box;
+  //evas_object_size_hint_weight_set(box, 1.0, 1.0);
+  //evas_object_size_hint_align_set(box, -1.0, -1.0);
+  evas_object_event_callback_add(box, EVAS_CALLBACK_FREE, _del_hook, wd);
+  evas_object_show(box);
+  
+  /* Roster */
+  
+  rl = elm_anchorview_add(parent);
+  evas_object_size_hint_weight_set(rl, 1.0, 1.0);
+  evas_object_size_hint_align_set(rl, -1.0, -1.0);
+  elm_anchorview_text_set(rl, "It's startup <a href=1>kayo@neko.im</a>");
+  //elm_win_resize_object_add(wn, rl);
+  elm_box_pack_end(box, rl);
+  evas_object_show(rl);
+  
+  
+  /* Buttons */
+  buttons = elm_box_add(parent);
+  elm_box_horizontal_set(buttons, 1);
+  elm_box_homogenous_set(buttons, 1);
+  evas_object_size_hint_weight_set(buttons, 1.0, 0.0);
+  evas_object_size_hint_align_set(buttons, -1.0, 1.0);
+  elm_box_pack_end(box, buttons);
+  evas_object_show(buttons);
+  
+  /* Status */
+  status = elm_hoversel_add(parent);
+  wd->status=status;
+  elm_hoversel_label_set(status, _("Status"));
+  elm_hoversel_hover_parent_set(status, box);
+  evas_object_size_hint_weight_set(status, 1.0, 1.0);
+  evas_object_size_hint_align_set(status, -1.0, 0.0);
+  
+  elm_hoversel_item_add(status, _("Offline"), NULL, 0, NULL, NULL);
+  elm_hoversel_item_add(status, _("Online"), NULL, 0, NULL, NULL);
+  elm_hoversel_item_add(status, _("Away"), NULL, 0, NULL, NULL);
+  elm_hoversel_item_add(status, _("Extended Away"), NULL, 0, NULL, NULL);
+  elm_hoversel_item_add(status, _("Do not disturb"), NULL, 0, NULL, NULL);
+  
+  elm_box_pack_end(buttons, status);
+  evas_object_show(status);
+  
+  /* Actions */
+  actions = elm_hoversel_add(parent);
+  elm_hoversel_label_set(actions, _("Actions"));
+  elm_hoversel_hover_parent_set(actions, box);
+  evas_object_size_hint_weight_set(actions, 1.0, 1.0);
+  evas_object_size_hint_align_set(actions, -1.0, 0.0);
+  
+  elm_hoversel_item_add(actions, _("Settings"), NULL, 0, _config_hook, wd);
+  elm_hoversel_item_add(actions, _("About"), NULL, 0, NULL, NULL);
+  elm_hoversel_item_add(actions, _("Exit"), NULL, 0, _exit_hook, NULL);
+  
+  elm_box_pack_end(buttons, actions);
+  evas_object_show(actions);
+  
+  return box;
+}
